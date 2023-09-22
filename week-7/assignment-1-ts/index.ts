6e89d24d852ebe8aa6c1a23e80cf5d804a4ee986
Neeraj Singh <869neerajsingh@gmail.com>
2023-09-22 09:11:23 +0530

    Checkpoint 1 - Rename JS files using .ts extension

---
+++ week-7/assignment-1-ts/index.ts
@@ -0,0 +1,19 @@
+
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const port = 3000;
+const authRoutes = require("./routes/auth");
+const todoRoutes = require("./routes/todo");
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+app.use("/auth", authRoutes);
+app.use("/todo", todoRoutes);
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+
+mongoose.connect('mongodb+srv://nsingh:M1voCmaVNGwgruws@cluster0.oibuqbf.mongodb.net/courses', { dbName: "courses" });
