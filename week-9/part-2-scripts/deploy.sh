68f4db2b5f675e91908e52ec0d28170a3b60a760
Neeraj Singh <869neerajsingh@gmail.com>
2023-10-16 12:56:12 +0530

    Add code for CD

--- week-9/part-2-scripts/deploy.sh
+++ week-9/part-2-scripts/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.0/bin
 
-cd Week-5-with-axios-and-backend
- git pull origin master
+cd assignments/week-9
+ git pull origin main
  cd server
- pm2 kill
- pm2 start index.js
+ /home/ubuntu/.nvm/versions/node/v20.5.1/bin/pm2 kill
+ /home/ubuntu/.nvm/versions/node/v20.5.1/bin/pm2 start index.js
--- week-9/part-2-scripts/script-local.sh
+++ week-9/part-2-scripts/script-local.sh
@@ -1,1 +1,1 @@
-ssh -t -i "cert-file.cer" ubuntu@ec2-url  "sudo bash ~/deploy.sh"
+ssh -t -i todo-app-practice-key.pem ubuntu@ec2-54-161-135-49.compute-1.amazonaws.com  "sudo bash ~/deploy.sh"
